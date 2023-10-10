bdf6fc92a8e7d0ef85466437cd7c1621b3db4639
Samson Ajulor <samsonajulor@gmail.com>
2023-10-10 23:54:53 +0100

    done

--- test/deployDiamond.t.sol
+++ test/deployDiamond.t.sol
@@ -9,6 +9,7 @@ import "../contracts/Diamond.sol";
 import "../contracts/facets/MarketPlaceFacet.sol";
 import {ERC721Facet} from "../contracts/facets/ERC721Facet.sol";
 
+
 import "./helpers/DiamondUtils.sol";
 import {
     Helpers
@@ -26,13 +27,12 @@ contract DiamondDeployer is DiamondUtils, IDiamondCut {
 
     Listing l;
     // OurNFT nft;
-    address dayo;
-    address motunrayo;
-    address tope;
 
-
-    uint256 privKeyA;
-    uint256 privKeyB;
+    uint256 privKeyA = uint256(keccak256(abi.encodePacked("dayo")));
+    uint256 privKeyB = uint256(keccak256(abi.encodePacked("motun")));
+    address dayo = vm.addr(privKeyA);
+    address motunrayo = vm.addr(privKeyB);
+    address tope = address(0x3333);
     // uint256 user
     function mkaddr(
         string memory name
@@ -71,18 +71,9 @@ contract DiamondDeployer is DiamondUtils, IDiamondCut {
         sig = bytes.concat(r, s, bytes1(v));
     }
 
-    function switchSigner(address _newSigner) public {
-        vm.startPrank(_newSigner);
-        vm.deal(_newSigner, 4 ether);
-    }
-
     function setUp() public {
-        // market place setup
-
-        (dayo, privKeyA) = mkaddr("DAYO");
-        (motunrayo, privKeyB) = mkaddr("MOTUN");
-
-        // switchSigner(dayo);
+        vm.deal(dayo, 4 ether);
+        vm.deal(motunrayo, 4 ether);
         //deploy facets
         dCutFacet = new DiamondCutFacet();
         diamond = new Diamond(address(this), address(dCutFacet), "MYNFT", "nft");
@@ -105,7 +96,7 @@ contract DiamondDeployer is DiamondUtils, IDiamondCut {
         });
 
         // mint NFT
-        erc721F.mint(dayo, 1);
+        erc721F.mint(address(0x1111), 1);
 
         //upgrade diamond with facets
 
@@ -149,6 +140,7 @@ contract DiamondDeployer is DiamondUtils, IDiamondCut {
 
         //call a function
         DiamondLoupeFacet(address(diamond)).facetAddresses();
+        // vm.stopPrank();
     }
 
     function diamondCut(
@@ -165,178 +157,179 @@ contract DiamondDeployer is DiamondUtils, IDiamondCut {
 
     /// Tests for MarketPlace
     function testOwnerCannotCreateListing() public {
-        l.lister = motunrayo;
-        switchSigner(motunrayo);
+        l.lister = dayo;
+        vm.startPrank(address(diamond));
 
         vm.expectRevert(MarketPlaceFacet.NotOwner.selector);
         marketF.createListing(l);
     }
 
     function testNonApprovedNFT() public {
-        switchSigner(dayo);
+        // start prank with creator
+        vm.startPrank(address(diamond));
         vm.expectRevert(MarketPlaceFacet.NotApproved.selector);
         marketF.createListing(l);
     }
 
-    // function testMinPriceTooLow() public {
-    //     switchSigner(dayo);
-    //     erc721F.setApprovalForAll(address(marketF), true);
-    //     l.price = 0;
-    //     vm.expectRevert(MarketPlaceFacet.MinPriceTooLow.selector);
-    //     marketF.createListing(l);
-    // }
+    function testMinPriceTooLow() public {
+        vm.startPrank(dayo);
+        erc721F.setApprovalForAll(address(marketF), true);
+        l.price = 0;
+        vm.expectRevert(MarketPlaceFacet.MinPriceTooLow.selector);
+        marketF.createListing(l);
+    }
 
-    // function testMinDeadline() public {
-    //     switchSigner(dayo);
-    //     erc721F.setApprovalForAll(address(marketF), true);
-    //     vm.expectRevert(MarketPlaceFacet.DeadlineTooSoon.selector);
-    //     marketF.createListing(l);
-    // }
+    function testMinDeadline() public {
+        vm.startPrank(dayo);
+        erc721F.setApprovalForAll(address(marketF), true);
+        vm.expectRevert(MarketPlaceFacet.DeadlineTooSoon.selector);
+        marketF.createListing(l);
+    }
 
-    // function testMinDuration() public {
-    //     switchSigner(dayo);
-    //     erc721F.setApprovalForAll(address(marketF), true);
-    //     l.deadline = uint88(block.timestamp + 59 minutes);
-    //     vm.expectRevert(MarketPlaceFacet.MinDurationNotMet.selector);
-    //     marketF.createListing(l);
-    // }
+    function testMinDuration() public {
+        vm.startPrank(dayo);
+        erc721F.setApprovalForAll(address(marketF), true);
+        l.deadline = uint88(block.timestamp + 59 minutes);
+        vm.expectRevert(MarketPlaceFacet.MinDurationNotMet.selector);
+        marketF.createListing(l);
+    }
 
-    // function testValidSig() public {
-    //     switchSigner(dayo);
-    //     erc721F.setApprovalForAll(address(marketF), true);
-    //     l.deadline = uint88(block.timestamp + 120 minutes);
-    //     l.sig = constructSig(
-    //         l.token,
-    //         l.tokenId,
-    //         l.price,
-    //         l.deadline,
-    //         l.lister,
-    //         privKeyB
-    //     );
-    //     vm.expectRevert(MarketPlaceFacet.InvalidSignature.selector);
-    //     marketF.createListing(l);
-    // }
+    function testValidSig() public {
+        vm.startPrank(dayo);
+        erc721F.setApprovalForAll(address(marketF), true);
+        l.deadline = uint88(block.timestamp + 120 minutes);
+        l.sig = constructSig(
+            l.token,
+            l.tokenId,
+            l.price,
+            l.deadline,
+            l.lister,
+            privKeyB
+        );
+        vm.expectRevert(MarketPlaceFacet.InvalidSignature.selector);
+        marketF.createListing(l);
+    }
 
-    // // EDIT LISTING
-    // function testEditNonValidListing() public {
-    //     switchSigner(dayo);
-    //     vm.expectRevert(MarketPlaceFacet.ListingNotExistent.selector);
-    //     marketF.editListing(1, 0, false);
-    // }
+    // EDIT LISTING
+    function testEditNonValidListing() public {
+        vm.startPrank(dayo);
+        vm.expectRevert(MarketPlaceFacet.ListingNotExistent.selector);
+        marketF.editListing(1, 0, false);
+    }
 
-    // function testEditListingNotOwner() public {
-    //     switchSigner(dayo);
-    //     erc721F.setApprovalForAll(address(marketF), true);
-    //     l.deadline = uint88(block.timestamp + 120 minutes);
-    //     l.sig = constructSig(
-    //         l.token,
-    //         l.tokenId,
-    //         l.price,
-    //         l.deadline,
-    //         l.lister,
-    //         privKeyA
-    //     );
-    //     uint256 lId = marketF.createListing(l);
-    //     switchSigner(motunrayo);
-    //     vm.expectRevert(MarketPlaceFacet.NotOwner.selector);
-    //     marketF.editListing(lId, 0, false);
-    // }
+    function testEditListingNotOwner() public {
+        vm.startPrank(dayo);
+        erc721F.setApprovalForAll(address(marketF), true);
+        l.deadline = uint88(block.timestamp + 120 minutes);
+        l.sig = constructSig(
+            l.token,
+            l.tokenId,
+            l.price,
+            l.deadline,
+            l.lister,
+            privKeyA
+        );
+        uint256 lId = marketF.createListing(l);
+        vm.startPrank(motunrayo);
+        vm.expectRevert(MarketPlaceFacet.NotOwner.selector);
+        marketF.editListing(lId, 0, false);
+    }
 
-    // function testEditListing() public {
-    //     switchSigner(dayo);
-    //     erc721F.setApprovalForAll(address(marketF), true);
-    //     l.deadline = uint88(block.timestamp + 120 minutes);
-    //     l.sig = constructSig(
-    //         l.token,
-    //         l.tokenId,
-    //         l.price,
-    //         l.deadline,
-    //         l.lister,
-    //         privKeyA
-    //     );
-    //     uint256 lId = marketF.createListing(l);
-    //     marketF.editListing(lId, 0.01 ether, false);
+    function testEditListing() public {
+        vm.startPrank(dayo);
+        erc721F.setApprovalForAll(address(marketF), true);
+        l.deadline = uint88(block.timestamp + 120 minutes);
+        l.sig = constructSig(
+            l.token,
+            l.tokenId,
+            l.price,
+            l.deadline,
+            l.lister,
+            privKeyA
+        );
+        uint256 lId = marketF.createListing(l);
+        marketF.editListing(lId, 0.01 ether, false);
 
-    //     Listing memory t = marketF.getListing(lId);
-    //     assertEq(t.price, 0.01 ether);
-    //     assertEq(t.active, false);
-    // }
+        Listing memory t = marketF.getListing(lId);
+        assertEq(t.price, 0.01 ether);
+        assertEq(t.active, false);
+    }
 
-    // // EXECUTE LISTING
-    // function testExecuteNonValidListing() public {
-    //     switchSigner(dayo);
-    //     vm.expectRevert(MarketPlaceFacet.ListingNotExistent.selector);
-    //     marketF.executeListing(1);
-    // }
+    // EXECUTE LISTING
+    function testExecuteNonValidListing() public {
+        vm.startPrank(dayo);
+        vm.expectRevert(MarketPlaceFacet.ListingNotExistent.selector);
+        marketF.executeListing(1);
+    }
 
-    // function testExecuteExpiredListing() public {
-    //     switchSigner(dayo);
-    //     erc721F.setApprovalForAll(address(marketF), true);
-    // }
+    function testExecuteExpiredListing() public {
+        vm.startPrank(dayo);
+        erc721F.setApprovalForAll(address(marketF), true);
+    }
 
-    // function testExecuteListingNotActive() public {
-    //     switchSigner(dayo);
-    //     erc721F.setApprovalForAll(address(marketF), true);
-    //     l.deadline = uint88(block.timestamp + 120 minutes);
-    //     l.sig = constructSig(
-    //         l.token,
-    //         l.tokenId,
-    //         l.price,
-    //         l.deadline,
-    //         l.lister,
-    //         privKeyA
-    //     );
-    //     uint256 lId = marketF.createListing(l);
-    //     marketF.editListing(lId, 0.01 ether, false);
-    //     vm.expectRevert(MarketPlaceFacet.ListingNotActive.selector);
-    //     marketF.executeListing(lId);
-    // }
+    function testExecuteListingNotActive() public {
+        vm.startPrank(dayo);
+        erc721F.setApprovalForAll(address(marketF), true);
+        l.deadline = uint88(block.timestamp + 120 minutes);
+        l.sig = constructSig(
+            l.token,
+            l.tokenId,
+            l.price,
+            l.deadline,
+            l.lister,
+            privKeyA
+        );
+        uint256 lId = marketF.createListing(l);
+        marketF.editListing(lId, 0.01 ether, false);
+        vm.expectRevert(MarketPlaceFacet.ListingNotActive.selector);
+        marketF.executeListing(lId);
+    }
 
-    // function testExecutePriceNotMet() public {
-    //     switchSigner(dayo);
-    //     erc721F.setApprovalForAll(address(marketF), true);
-    //     l.deadline = uint88(block.timestamp + 120 minutes);
-    //     l.sig = constructSig(
-    //         l.token,
-    //         l.tokenId,
-    //         l.price,
-    //         l.deadline,
-    //         l.lister,
-    //         privKeyA
-    //     );
-    //     uint256 lId = marketF.createListing(l);
-    //     vm.expectRevert(
-    //         abi.encodeWithSelector(
-    //             MarketPlaceFacet.PriceNotMet.selector,
-    //             l.price - 0.9 ether
-    //         )
-    //     );
-    //     marketF.executeListing{value: 0.9 ether}(lId);
-    // }
+    function testExecutePriceNotMet() public {
+        vm.startPrank(dayo);
+        erc721F.setApprovalForAll(address(marketF), true);
+        l.deadline = uint88(block.timestamp + 120 minutes);
+        l.sig = constructSig(
+            l.token,
+            l.tokenId,
+            l.price,
+            l.deadline,
+            l.lister,
+            privKeyA
+        );
+        uint256 lId = marketF.createListing(l);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                MarketPlaceFacet.PriceNotMet.selector,
+                l.price - 0.9 ether
+            )
+        );
+        marketF.executeListing{value: 0.9 ether}(lId);
+    }
 
 
-    // function testExecute() public {
-    //     switchSigner(dayo);
-    //     erc721F.setApprovalForAll(address(marketF), true);
-    //     l.deadline = uint88(block.timestamp + 120 minutes);
-    //     l.sig = constructSig(
-    //         l.token,
-    //         l.tokenId,
-    //         l.price,
-    //         l.deadline,
-    //         l.lister,
-    //         privKeyA
-    //     );
-    //     uint256 lId = marketF.createListing(l);
-    //     switchSigner(motunrayo);
-    //     uint256 dayoBalanceBefore = dayo.balance;
+    function testExecute() public {
+        vm.startPrank(dayo);
+        erc721F.setApprovalForAll(address(marketF), true);
+        l.deadline = uint88(block.timestamp + 120 minutes);
+        l.sig = constructSig(
+            l.token,
+            l.tokenId,
+            l.price,
+            l.deadline,
+            l.lister,
+            privKeyA
+        );
+        uint256 lId = marketF.createListing(l);
+        vm.startPrank(motunrayo);
+        uint256 dayoBalanceBefore = dayo.balance;
 
-    //     marketF.executeListing{value: l.price}(lId);
+        marketF.executeListing{value: l.price}(lId);
 
-    //     Listing memory t = marketF.getListing(lId);
-    //     assertEq(t.price, 1 ether);
-    //     assertEq(t.active, false);
+        Listing memory t = marketF.getListing(lId);
+        assertEq(t.price, 1 ether);
+        assertEq(t.active, false);
 
-    //     assertEq(ERC721(l.token).ownerOf(l.tokenId), motunrayo);
-    // }
+        assertEq(ERC721(l.token).ownerOf(l.tokenId), motunrayo);
+    }
 }
